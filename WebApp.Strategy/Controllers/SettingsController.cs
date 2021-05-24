bea43715097f3c63a0c4368c29a06da387770e18
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-05-25 00:24:19 +0300

    7.adım

--- WebApp.Strategy/Controllers/SettingsController.cs
+++ WebApp.Strategy/Controllers/SettingsController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BaseProject.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using WebApp.Strategy.Models;
 
@@ -11,6 +15,15 @@ namespace WebApp.Strategy.Controllers
     [Authorize]
     public class SettingsController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public SettingsController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
 
@@ -34,6 +47,45 @@ namespace WebApp.Strategy.Controllers
 
 
             return View(settings);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangeDatabase(int databaseType)
+        {
+
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+
+            var newClaim = new Claim(Settings.claimDatabaseType, databaseType.ToString());
+
+
+            var claims = await _userManager.GetClaimsAsync(user);
+
+            var hasDatabaseTypeClaim = claims.FirstOrDefault(x => x.Type == Settings.claimDatabaseType);
+
+
+            if(hasDatabaseTypeClaim!=null)
+            {
+
+                await _userManager.ReplaceClaimAsync(user, hasDatabaseTypeClaim, newClaim);
+            }
+            else
+            {
+                await _userManager.AddClaimAsync(user, newClaim);
+            }
+
+
+            await _signInManager.SignOutAsync();
+
+           var authenticateResult= await HttpContext.AuthenticateAsync();
+
+
+            await _signInManager.SignInAsync(user, authenticateResult.Properties);
+
+            return RedirectToAction(nameof(Index));
+
+
+
         }
     }
 }
