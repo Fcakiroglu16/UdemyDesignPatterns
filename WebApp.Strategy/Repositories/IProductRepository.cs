a7d33035451d351cf6fd15efcf9e70acc9a77729
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-05-24 21:24:39 +0300

    2.adım

---
+++ WebApp.Strategy/Repositories/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Strategy.Models;
+
+namespace WebApp.Strategy.Repositories
+{
+    interface IProductRepository
+    {
+
+        Task<Product> GetById(string id);
+
+        Task<List<Product>> GetAllByUserId(string userId);
+
+        Task<Product> Save(Product product);
+        Task Update(Product product);
+        Task Delete(Product product);
+
+
+
+    }
+}
