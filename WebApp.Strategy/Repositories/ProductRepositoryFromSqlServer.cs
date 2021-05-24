0e35ee401e1efc237c5b63d674722e040e6493b1
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-05-24 21:35:22 +0300

    3.adım

---
+++ WebApp.Strategy/Repositories/ProductRepositoryFromSqlServer.cs
@@ -0,0 +1,56 @@
+﻿using BaseProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Strategy.Models;
+
+namespace WebApp.Strategy.Repositories
+{
+    public class ProductRepositoryFromSqlServer : IProductRepository
+    {
+        private readonly AppIdentityDbContext _context;
+        public ProductRepositoryFromSqlServer(AppIdentityDbContext appIdentityDbContext)
+        {
+            _context = appIdentityDbContext;
+        }
+
+        public async Task Delete(Product product)
+        {
+
+           // _context.Entry(product).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+            _context.Products.Remove(product);
+
+            await _context.SaveChangesAsync();
+
+        }
+
+        public async Task<List<Product>> GetAllByUserId(string userId)
+        {
+            return await _context.Products.Where(x => x.UserId == userId).ToListAsync();
+        }
+
+        public async Task<Product> GetById(string id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task<Product> Save(Product product)
+        {
+
+            await _context.Products.AddAsync(product);
+
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task Update(Product product)
+        {
+            _context.Products.Update(product);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- WebApp.Strategy/Startup.cs
+++ WebApp.Strategy/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApp.Strategy.Repositories;
 
 namespace BaseProject
 {
@@ -26,6 +27,8 @@ namespace BaseProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+         
 
             services.AddDbContext<AppIdentityDbContext>(options =>
             {
