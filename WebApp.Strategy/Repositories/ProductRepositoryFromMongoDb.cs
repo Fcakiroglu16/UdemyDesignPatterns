7fc4be1c9820f9b8d29a18ec5ce11a459e1f58c1
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-05-24 23:07:45 +0300

    4.adım

---
+++ WebApp.Strategy/Repositories/ProductRepositoryFromMongoDb.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Strategy.Models;
+
+namespace WebApp.Strategy.Repositories
+{
+    public class ProductRepositoryFromMongoDb : IProductRepository
+    {
+        private readonly IMongoCollection<Product> _productCollection;
+
+        public ProductRepositoryFromMongoDb(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("MongoDb");
+
+            var client = new MongoClient(connectionString);
+
+            var database = client.GetDatabase("ProductDb");
+
+            _productCollection = database.GetCollection<Product>("Products");
+        }
+
+        public async Task Delete(Product product)
+        {
+            await _productCollection.DeleteOneAsync(x => x.Id == product.Id);
+        }
+
+        public async Task<List<Product>> GetAllByUserId(string userId)
+        {
+            return await _productCollection.Find(x => x.UserId == userId).ToListAsync();
+        }
+
+        public async Task<Product> GetById(string id)
+        {
+            return await _productCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<Product> Save(Product product)
+        {
+            await _productCollection.InsertOneAsync(product);
+
+            return product;
+        }
+
+        public async Task Update(Product product)
+        {
+            await _productCollection.FindOneAndReplaceAsync(x => x.Id == product.Id, product);
+        }
+    }
+}
