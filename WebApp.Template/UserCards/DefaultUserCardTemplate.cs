8125623ae8fb7c9c9abab150bd4ecf76a687840a
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-05-26 00:26:37 +0300

    adım 1/2

---
+++ WebApp.Template/UserCards/DefaultUserCardTemplate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Template.UserCards
+{
+    public class DefaultUserCardTemplate : UserCardTemplate
+    {
+        protected override string SetFooter()
+        {
+            return string.Empty;
+        }
+
+        protected override string SetPicture()
+        {
+            return $"<img class='card-img-top' src='/userpictures/defaultuserpicture.png'>";
+        }
+    }
+}
---
+++ WebApp.Template/UserCards/UserCardTemplate.cs
@@ -0,0 +1,41 @@
+﻿using BaseProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Template.UserCards
+{
+    public abstract class UserCardTemplate
+    {
+        protected AppUser AppUser { get; set; }
+
+        public void SetUser(AppUser appUser)
+        {
+            AppUser = appUser;
+        }
+
+        public string Build()
+        {
+            if (AppUser == null) throw new ArgumentNullException(nameof(AppUser));
+
+            var sb = new StringBuilder();
+
+            sb.Append("<div class='card'>");
+            sb.Append(SetPicture());
+            sb.Append($@"<div class='card-body'>
+                          <h5>{AppUser.UserName}</h5>
+                          <p>{AppUser.Description}</p>");
+            sb.Append(SetFooter());
+            sb.Append("</div>");
+            sb.Append("</div>");
+
+            return sb.ToString();
+        }
+
+        protected abstract string SetFooter();
+
+        protected abstract string SetPicture();
+    }
+}
