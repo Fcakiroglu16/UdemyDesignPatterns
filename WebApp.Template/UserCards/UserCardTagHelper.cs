a44363457f519d7f560388c2710ce04abf6d40fd
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-05-26 01:01:06 +0300

    adım 4

---
+++ WebApp.Template/UserCards/UserCardTagHelper.cs
@@ -0,0 +1,40 @@
+﻿using BaseProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Template.UserCards
+{
+    public class UserCardTagHelper : TagHelper
+    {
+        public AppUser AppUser { get; set; }
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserCardTagHelper(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            UserCardTemplate userCardTemplate;
+
+            if (_httpContextAccessor.HttpContext.User.Identity.IsAuthenticated)
+            {
+                userCardTemplate = new PrimeUserCardTemplate();
+            }
+            else
+            {
+                userCardTemplate = new DefaultUserCardTemplate();
+            }
+
+            userCardTemplate.SetUser(AppUser);
+
+            output.Content.SetHtmlContent(userCardTemplate.Build());
+        }
+    }
+}
