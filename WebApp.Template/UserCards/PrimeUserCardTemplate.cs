40f74325fcda87ec4f6079b17dbdfc876d855e5e
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-05-26 00:34:03 +0300

    adım 3

---
+++ WebApp.Template/UserCards/PrimeUserCardTemplate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Template.UserCards
+{
+    public class PrimeUserCardTemplate : UserCardTemplate
+    {
+        protected override string SetFooter()
+        {
+            var sb = new StringBuilder();
+
+            sb.Append("<a href='#' class='card-link'>Mesaj gönder</a>");
+            sb.Append("<a href='#' class='card-link'>Detaylı profil</a>");
+            return sb.ToString();
+        }
+
+        protected override string SetPicture()
+        {
+            return $"<img class='card-img-top' src='{AppUser.PictureUrl}'>";
+        }
+    }
+}
