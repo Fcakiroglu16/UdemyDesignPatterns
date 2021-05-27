e23877d15c891725a49f40337738787df8917991
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-05-28 02:21:00 +0300

    command pattern 7.adım

--- WebApp.Command/Controllers/ProductsController.cs
+++ WebApp.Command/Controllers/ProductsController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApp.Command.Commands;
@@ -51,6 +53,39 @@ namespace WebApp.Command.Controllers
             }
 
             return fileCreateInvoker.CreateFile();
+        }
+
+        public async Task<IActionResult> CreateFiles()
+        {
+            var products = await _context.Products.ToListAsync();
+            ExcelFile<Product> excelFile = new(products);
+            PdfFile<Product> pdfFile = new(products, HttpContext);
+            FileCreateInvoker fileCreateInvoker = new();
+
+            fileCreateInvoker.AddCommand(new CreateExcelTableActionCommand<Product>(excelFile));
+            fileCreateInvoker.AddCommand(new CreatePdfTableActionCommand<Product>(pdfFile));
+
+            var filesResult = fileCreateInvoker.CreateFiles();
+
+            using (var zipMemoryStream = new MemoryStream())
+            {
+                using (var archive = new ZipArchive(zipMemoryStream, ZipArchiveMode.Create))
+                {
+                    foreach (var item in filesResult)
+                    {
+                        var fileContent = item as FileContentResult;
+
+                        var zipFile = archive.CreateEntry(fileContent.FileDownloadName);
+
+                        using (var zipEntryStream = zipFile.Open())
+                        {
+                            await new MemoryStream(fileContent.FileContents).CopyToAsync(zipEntryStream);
+                        }
+                    }
+                }
+
+                return File(zipMemoryStream.ToArray(), "application/zip", "all.zip");
+            }
         }
     }
 }
