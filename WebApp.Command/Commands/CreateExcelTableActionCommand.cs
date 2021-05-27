7a39987fcee709e8112344e8faa69eab7ed415ae
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-05-28 00:59:30 +0300

    command pattern 4.adım

---
+++ WebApp.Command/Commands/CreateExcelTableActionCommand.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Command.Commands
+{
+    public class CreateExcelTableActionCommand<T> : ITableActionCommand
+    {
+        private readonly ExcelFile<T> _excelFile;
+
+        public CreateExcelTableActionCommand(ExcelFile<T> excelFile)
+        {
+            _excelFile = excelFile;
+        }
+
+        public IActionResult Execute()
+        {
+            var excelMemoryStream = _excelFile.Create();
+
+            return new FileContentResult(excelMemoryStream.ToArray(), _excelFile.FileType) { FileDownloadName = _excelFile.FileName };
+        }
+    }
+}
---
+++ WebApp.Command/Commands/CreatePdfTableActionCommand.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Command.Commands
+{
+    public class CreatePdfTableActionCommand<T> : ITableActionCommand
+    {
+        private readonly PdfFile<T> _pdfFile;
+
+        public CreatePdfTableActionCommand(PdfFile<T> pdfFile)
+        {
+            _pdfFile = pdfFile;
+        }
+
+        public IActionResult Execute()
+        {
+            var excelMemoryStream = _pdfFile.Create();
+
+            return new FileContentResult(excelMemoryStream.ToArray(), _pdfFile.FileType) { FileDownloadName = _pdfFile.FileName };
+        }
+    }
+}
