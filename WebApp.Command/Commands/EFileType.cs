0afce3fe3fda01a63da65dbec9d0d1ae8ff3c9a0
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-05-28 01:48:24 +0300

    command pattern 6.adım

---
+++ WebApp.Command/Commands/EFileType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Command.Commands
+{
+    public enum EFileType
+    {
+        Excel = 1,
+        Pdf = 2
+    }
+}
---
+++ WebApp.Command/Controllers/ProductsController.cs
@@ -0,0 +1,56 @@
+﻿using BaseProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Command.Commands;
+using WebApp.Command.Models;
+
+namespace WebApp.Command.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly AppIdentityDbContext _context;
+
+        public ProductsController(AppIdentityDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+
+        public async Task<IActionResult> CreateFile(int type)
+        {
+            var products = await _context.Products.ToListAsync();
+
+            FileCreateInvoker fileCreateInvoker = new();
+
+            EFileType fileType = (EFileType)type;
+
+            switch (fileType)
+            {
+                case EFileType.Excel:
+                    ExcelFile<Product> excelFile = new(products);
+
+                    fileCreateInvoker.SetCommand(new CreateExcelTableActionCommand<Product>(excelFile));
+
+                    break;
+
+                case EFileType.Pdf:
+                    PdfFile<Product> pdfFile = new(products, HttpContext);
+                    fileCreateInvoker.SetCommand(new CreatePdfTableActionCommand<Product>(pdfFile));
+                    break;
+
+                default:
+                    break;
+            }
+
+            return fileCreateInvoker.CreateFile();
+        }
+    }
+}
