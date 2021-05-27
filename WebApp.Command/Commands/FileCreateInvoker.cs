0b20e2a20a0addde37905de31028162447896b04
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-05-28 01:13:39 +0300

    command pattern 5.adım

---
+++ WebApp.Command/Commands/FileCreateInvoker.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Command.Commands
+{
+    public class FileCreateInvoker
+    {
+        private ITableActionCommand _tableActionCommand;
+        private List<ITableActionCommand> tableActionCommands = new List<ITableActionCommand>();
+
+        public void SetCommand(ITableActionCommand tableActionCommand)
+        {
+            _tableActionCommand = tableActionCommand;
+        }
+
+        public void AddCommand(ITableActionCommand tableActionCommand)
+        {
+            tableActionCommands.Add(tableActionCommand);
+        }
+
+        public IActionResult CreateFile()
+        {
+            return _tableActionCommand.Execute();
+        }
+
+        public List<IActionResult> CreateFiles()
+        {
+            return tableActionCommands.Select(x => x.Execute()).ToList();
+        }
+    }
+}
