8685ed35bcd047cd26b2665a3864f4a6601c597c
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-05-27 23:38:23 +0300

    command pattern 2.adım

---
+++ WebApp.Command/Commands/ExcelFile.cs
@@ -0,0 +1,57 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Command.Commands
+{
+    public class ExcelFile<T>
+    {
+        public readonly List<T> _list;
+
+        public string FileName => $"{typeof(T).Name}.xlsx";
+
+        public string FileType => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+        public ExcelFile(List<T> list)
+        {
+            _list = list;
+        }
+
+        public MemoryStream Create()
+        {
+            var wb = new XLWorkbook();
+
+            var ds = new DataSet();
+
+            ds.Tables.Add(GetTable());
+
+            wb.Worksheets.Add(ds);
+
+            var excelMemory = new MemoryStream();
+
+            wb.SaveAs(excelMemory);
+            return excelMemory;
+        }
+
+        private DataTable GetTable()
+        {
+            var table = new DataTable();
+
+            var type = typeof(T);
+
+            type.GetProperties().ToList().ForEach(x => table.Columns.Add(x.Name, x.PropertyType));
+
+            _list.ForEach(x =>
+            {
+                var values = type.GetProperties().Select(properyInfo => properyInfo.GetValue(x, null)).ToArray();
+
+                table.Rows.Add(values);
+            });
+            return table;
+        }
+    }
+}
--- WebApp.Command/Controllers/HomeController.cs
+++ WebApp.Command/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApp.Command.Commands;
+using WebApp.Command.Models;
 
 namespace BaseProject.Controllers
 {
