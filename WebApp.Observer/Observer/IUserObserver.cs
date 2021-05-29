b88fd1f01d8faba8ef85b0ffdf968c14338f20c5
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-05-29 18:51:14 +0300

    observer pattern 2.adım

---
+++ WebApp.Observer/Observer/IUserObserver.cs
@@ -0,0 +1,13 @@
+﻿using BaseProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Observer.Observer
+{
+    internal interface IUserObserver
+    {
+        void CreateUser(AppUser appUser);
+    }
+}
---
+++ WebApp.Observer/Observer/UserObserverWriteToConsole.cs
@@ -0,0 +1,29 @@
+﻿using BaseProject.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Observer.Observer
+{
+    public class UserObserverWriteToConsole : IUserObserver
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public UserObserverWriteToConsole(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void CreateUser(AppUser appUser)
+        {
+            var logger = _serviceProvider.GetRequiredService<ILogger<UserObserverWriteToConsole>>();
+
+            logger.LogInformation($"user created : Id= {appUser.Id}");
+
+            throw new NotImplementedException();
+        }
+    }
+}
