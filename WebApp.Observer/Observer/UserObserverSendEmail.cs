623e3644c3d1808ca39bf182ea8a77e8b5937b22
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-05-29 19:09:39 +0300

    observer pattern 4.adım

---
+++ WebApp.Observer/Observer/UserObserverSendEmail.cs
@@ -0,0 +1,46 @@
+﻿using BaseProject.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace WebApp.Observer.Observer
+{
+    public class UserObserverSendEmail : IUserObserver
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public UserObserverSendEmail(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void CreateUser(AppUser appUser)
+        {
+            var logger = _serviceProvider.GetRequiredService<ILogger<UserObserverSendEmail>>();
+
+            var mailMessage = new MailMessage();
+
+            var smptClient = new SmtpClient("srvm11.trwww.com");
+
+            mailMessage.From = new MailAddress("deneme@kariyersistem.com");
+
+            mailMessage.To.Add(new MailAddress(appUser.Email));
+
+            mailMessage.Subject = "Sitemize hoş geldiniz.";
+
+            mailMessage.Body = "<p>Sitemizin genel kuralları : bıdı bıdı....</p>";
+
+            mailMessage.IsBodyHtml = true;
+            smptClient.Port = 587;
+            smptClient.Credentials = new NetworkCredential("deneme@kariyersistem.com", "Password12*");
+
+            smptClient.Send(mailMessage);
+            logger.LogInformation($"Email was send to user :{appUser.UserName}");
+        }
+    }
+}
